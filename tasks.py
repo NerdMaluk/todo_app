cb33c61ac57ec3e512fbabedeaece9297f35d0c3
Flavio Soares <nerdmaluk@gmail.com>
2024-05-30 18:01:24 -0100

    reset

--- tasks.py
+++ tasks.py
@@ -1,14 +1,20 @@
-tasks = []
-
-def add_task(task):
-    tasks.append({"task": task, "completed": False})
+def remove_task(index):
+    if 0 >= index < len(tasks):
+        tasks.pop(index)
+    else:
+        print("Índice inválido")
 
 def main():
     while True:
-        command = input("Comando (add/exit): ")
+        command = input("Comando (add/list/remove/exit): ")
         if command == "add":
             task = input("Tarefa: ")
             add_task(task)
+        elif command == "list":
+            list_tasks()
+        elif command == "remove":
+            index = int(input("Índice da tarefa a remover: ")) - 1
+            remove_task(index)
         elif command == "exit":
             break
 
